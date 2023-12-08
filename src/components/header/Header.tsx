1dfb23266646a8a698eb5421db565563bb6d7084
Iryna Zhebryk <irinazhebrik@gmail.com>
2023-12-08 10:40:46 +0100

    feat: refactor styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,27 @@
-import './Header.scss';
-
 import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { AppContext } from '../../context/ContextProvider';
 import LanguageToggler from '../langToggler/LangToggler';
+import { Button, Grid } from '@mui/material';
 
 const Header = () => {
-  const { isUser, logout } = useContext(AppContext);
+  const { isUser, logout, lang } = useContext(AppContext);
 
   return (
     <header>
-      <NavLink to="/">Welcome</NavLink>
-      <LanguageToggler />
-      <span>{isUser ? 'AUTH' : 'GUEST'}</span>
-      <button onClick={logout} disabled={isUser ? false : true}>
-        Log Out
-      </button>
+      <Grid container className="items-center h-14 p-2 gap-24 bg-gray-200">
+        <NavLink to="/">Welcome</NavLink>
+        <LanguageToggler />
+
+        <Button onClick={logout} variant="outlined" disabled={isUser ? false : true}>
+          Log Out
+        </Button>
+
+        <p>
+          Current:
+          <span>{isUser ? '   auth' : '   guest'}</span> <span>{lang}</span>
+        </p>
+      </Grid>
     </header>
   );
 };
