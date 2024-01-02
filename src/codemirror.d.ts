09e3ff9550e5c36ebf049b298e5defe74f15d78e
johngaalt <rajon777666@gmail.com>
2024-01-02 22:14:42 +0300

    refactor: codemirror modules

---
+++ src/codemirror.d.ts
@@ -0,0 +1,2 @@
+declare module '@codemirror/theme-one-dark';
+declare module '@uiw/codemirror-theme-okaidia';
--- src/components/queryEditor/QueryEditor.tsx
+++ src/components/queryEditor/QueryEditor.tsx
@@ -7,7 +7,6 @@ import CodeMirror from '@uiw/react-codemirror';
 import { graphql } from 'cm6-graphql';
 import { syntaxHighlighting } from '@codemirror/language';
 import { oneDarkHighlightStyle } from '@codemirror/theme-one-dark';
-import { autocompletion } from '@codemirror/autocomplete';
 import { okaidia } from '@uiw/codemirror-theme-okaidia';
 import { prettifyQuery } from './QueryEditor.utils';
 
@@ -51,7 +50,7 @@ const QueryEditor: React.FC = () => {
       <CodeMirror
         height="350px"
         theme={okaidia}
-        extensions={[graphql(), syntaxHighlighting(oneDarkHighlightStyle), autocompletion()]}
+        extensions={[graphql(), syntaxHighlighting(oneDarkHighlightStyle)]}
         placeholder="Type your GraphQL query here"
         value={query}
         onChange={(value) => setQuery(value)}
