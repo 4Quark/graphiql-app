f0d8563bcf1c131a42d7a7c822fbcd2577ea9cfb
Iryna Zhebryk <irinazhebrik@gmail.com>
2024-01-03 08:41:48 +0100

    fix: update app

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,14 +3,12 @@ import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 import AppContextProvider from '../context/ContextProvider';
-import Message from '../components/message/Message';
 
 export const App = () => {
   return (
     <ErrorBoundary>
       <AppContextProvider>
         <Header />
-        <Message />
         <div className="min-h-screen flex flex-col justify-center items-center pt-10">
           <Outlet />
         </div>
