048c33e9c93e48a9e1ba33bd40e580096c767a60
4Quark <mariya_baranova@list.ru>
2024-01-17 23:14:26 +0300

    fix: auth localization

--- src/services/auth/assets/schema.ts
+++ src/services/auth/assets/schema.ts
@@ -1,58 +1,23 @@
 import * as yup from 'yup';
 import { IForm, LangType } from '../../../types/interface';
 import { VALIDATION_PATTERN } from './utils/patterns';
+import { dictionary } from '../../localization/useLanguage';
 
-export const schemaDictionary = {
-  schemaEmail: {
-    ru: 'Неверный формат электронной почты',
-    en: 'Invalid email format',
-  },
-
-  schemaRequierd: {
-    ru: 'Обязательное поле для заполнения!',
-    en: 'Field is required!',
-  },
-
-  schemaLength: {
-    ru: 'Минимальная длина пароля: 6 символов',
-    en: 'Password must be at least 6 characters',
-  },
-
-  schemaPassNum: {
-    ru: 'Пароль должен содержать хотя бы одну цифру',
-    en: 'The password should contain at least one number',
-  },
-
-  schemaPassUp: {
-    ru: 'Пароль должен содержать хотя бы одну заглавную букву',
-    en: 'The password should contain at least one uppercase letter',
-  },
-
-  schemaPassLow: {
-    ru: 'Пароль должен содержать хотя бы одну строчную букву',
-    en: 'The password should contain at least one lowercase letter',
-  },
-  schemaPassSpec: {
-    ru: 'Пароль должен содержать хотя бы один специальный символ',
-    en: 'The password should contain at least one special character',
-  },
-};
-
-export const getSchema = (language: LangType) => {
+export const getSchema = (lang: LangType) => {
   const schema: yup.ObjectSchema<IForm> = yup.object({
     email: yup
       .string()
-      .email(schemaDictionary['schemaEmail'][language])
-      .matches(VALIDATION_PATTERN.EMAIL, schemaDictionary['schemaEmail'][language])
-      .required(schemaDictionary['schemaRequierd'][language]),
+      .email(dictionary.schemaEmail[lang])
+      .matches(VALIDATION_PATTERN.EMAIL, dictionary.schemaEmail[lang])
+      .required(dictionary.schemaRequierd[lang]),
     password: yup
       .string()
-      .min(6, schemaDictionary['schemaLength'][language])
-      .required(schemaDictionary['schemaRequierd'][language])
-      .matches(VALIDATION_PATTERN.PASS_NUM, schemaDictionary['schemaPassNum'][language])
-      .matches(VALIDATION_PATTERN.PASS_UPPER, schemaDictionary['schemaPassUp'][language])
-      .matches(VALIDATION_PATTERN.PASS_LOWER, schemaDictionary['schemaPassLow'][language])
-      .matches(VALIDATION_PATTERN.PASS_SPEC_CHAR, schemaDictionary['schemaPassSpec'][language]),
+      .min(6, dictionary.schemaLength[lang])
+      .required(dictionary.schemaRequierd[lang])
+      .matches(VALIDATION_PATTERN.PASS_NUM, dictionary.schemaPassNum[lang])
+      .matches(VALIDATION_PATTERN.PASS_UPPER, dictionary.schemaPassUp[lang])
+      .matches(VALIDATION_PATTERN.PASS_LOWER, dictionary.schemaPassLow[lang])
+      .matches(VALIDATION_PATTERN.PASS_SPEC_CHAR, dictionary.schemaPassSpec[lang]),
   });
   return schema;
 };
--- src/services/localization/useLanguage.ts
+++ src/services/localization/useLanguage.ts
@@ -245,6 +245,39 @@ export const dictionary = {
     ru: 'Введен некорректный заголовок',
     en: 'Incorrect Header entered',
   },
+  schemaEmail: {
+    ru: 'Неверный формат электронной почты',
+    en: 'Invalid email format',
+  },
+
+  schemaRequierd: {
+    ru: 'Обязательное поле для заполнения!',
+    en: 'Field is required!',
+  },
+
+  schemaLength: {
+    ru: 'Минимальная длина пароля: 6 символов',
+    en: 'Password must be at least 6 characters',
+  },
+
+  schemaPassNum: {
+    ru: 'Пароль должен содержать хотя бы одну цифру',
+    en: 'The password should contain at least one number',
+  },
+
+  schemaPassUp: {
+    ru: 'Пароль должен содержать хотя бы одну заглавную букву',
+    en: 'The password should contain at least one uppercase letter',
+  },
+
+  schemaPassLow: {
+    ru: 'Пароль должен содержать хотя бы одну строчную букву',
+    en: 'The password should contain at least one lowercase letter',
+  },
+  schemaPassSpec: {
+    ru: 'Пароль должен содержать хотя бы один специальный символ',
+    en: 'The password should contain at least one special character',
+  },
 };
 
 export const useLanguage = (key: DictionaryKey) => {
